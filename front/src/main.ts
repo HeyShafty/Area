be2da42bb18885a160116ce3fe3ed8c801993ad2
Kylian Balan <kylian.balan@epitech.eu>
2021-01-18 12:16:59 +0100

    [FRONT] bootstrapping project with tailwind css.

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,13 +1,5 @@
-import Vue from 'vue';
-import App from './App.vue';
-import './registerServiceWorker';
-import router from './router';
-import store from './store';
+import { createApp } from 'vue'
+import App from './App.vue'
+import './css/index.css'
 
-Vue.config.productionTip = false;
-
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app');
+createApp(App).mount('#app')
