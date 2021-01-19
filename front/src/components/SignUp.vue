7a703774388879fdee43c16cb7ef43d410cc15f5
SEBAÏ Ghassane <ghassane.sebai@epitech.eu>
2021-01-19 11:43:37 +0100

    [FIX] Cleaned up some code

--- front/src/components/SignUp.vue
+++ front/src/components/SignUp.vue
@@ -8,7 +8,7 @@
 import { ref, defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'HelloWorld',
+  name: 'SignUp',
   setup: () => {
     const count = ref(0)
     return { count }
