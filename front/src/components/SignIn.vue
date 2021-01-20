a10a1968f03cb13c9a55fbbdad0690d4721d74bb
Eliott Pal <eliott.palueau@epitech.eu>
2021-01-20 18:23:27 +0100

    [ADD] Page SignUp et SignIn incroyables, todo: link with back server & navigation to sign up page

--- front/src/components/SignIn.vue
+++ front/src/components/SignIn.vue
@@ -8,47 +8,57 @@
         class="mx-auto overflow-hidden bg-white rounded-md shadow-lg max-w-md"
       >
         <div class="p-5 md:flex-1">
+          <!-- CARD HEADER -->
           <h3 class="my-4 text-2xl font-semibold text-gray-700">Sign-In to your account</h3>
           <form action="#" class="flex flex-col space-y-5">
+            <!-- EMAIL INPUT -->
             <div class="flex flex-col space-y-1">
               <div class="flex items-center justify-between">
-                <label for="password" class="text-sm font-semibold text-gray-500">Email</label>
+                <label for="password" class="text-md font-semibold text-gray-500">Email</label>
               </div>
               <input
-                type="email"
-                id="email"
+                type="text"
+                v-model="email"
                 autofocus
                 class="px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"
               />
+              <span v-if="errorMessages.email" class="text-sm font-semibold text-red-500">{{errorMessages.email}}</span>
             </div>
+            <!-- PASSWORD INPUT -->
             <div class="flex flex-col space-y-1">
               <div class="flex items-center justify-between">
-                <label for="password" class="text-sm font-semibold text-gray-500">Password</label>
+                <label for="password" class="text-md font-semibold text-gray-500">Password</label>
               </div>
               <input
                 type="password"
-                id="password"
+                v-model="password"
                 class="px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"
               />
+              <span v-if="errorMessages.password" class="text-sm font-semibold text-red-500">{{errorMessages.password}}</span>
             </div>
+            <!-- SIGNIN BUTTON -->
             <div>
               <button
-                type="submit"
-                class="w-full px-4 py-2 text-lg font-semibold text-white transition-colors duration-300 bg-blue-500 rounded-md shadow hover:bg-blue-600 focus:outline-none focus:ring-blue-200 focus:ring-4"
+                type="button"
+                v-on:click="signIn"
+                v-bind:disabled="checkInputs() == false"
+                class="disabled:opacity-40 w-full px-4 py-2 text-lg font-semibold text-white transition-colors duration-300 bg-blue-500 rounded-md shadow hover:bg-blue-600 focus:outline-none focus:ring-blue-200 focus:ring-4"
               >
                 Sign-In
               </button>
             </div>
+            <!-- "OR LOGIN WITH" DIVIDER -->
             <div class="flex flex-col space-y-5">
               <span class="flex items-center justify-center space-x-2">
                 <span class="h-px bg-gray-400 w-14"></span>
                 <span class="font-normal text-gray-500">or sign-in with</span>
                 <span class="h-px bg-gray-400 w-14"></span>
               </span>
+              <!-- OFFICE LOGIN BUTTON -->
               <div class="flex flex-col space-y-4">
                 <a
-                  href="#"
-                  class="flex items-center justify-center px-4 py-2 space-x-2 transition-colors duration-300 border border-blue-500 rounded-md group hover:bg-blue-500 focus:outline-none"
+                  v-on:click="officeSignIn"
+                  class="flex items-center cursor-pointer justify-center py-2 space-x-2 transition-colors duration-300 border border-blue-500 rounded-md group hover:bg-blue-500 focus:outline-none"
                 >
                   <span>
                     <!-- OFFICE ICON -->
@@ -60,8 +70,14 @@
                       <path style="fill:#FFC107;" d="M272,272v240h224c8.832,0,16-7.168,16-16V272H272z"/>
                     </svg>
                   </span>
-                  <span class="text-sm font-medium text-blue-500 group-hover:text-white">Office</span>
+                  <span class="text-sm mt-1 font-medium text-blue-500 group-hover:text-white">Office</span>
                 </a>
+                <!-- REGISTER REDIRECT -->
+                <span class="h-px"></span>
+                <div class="container mx-auto space-y-0.5">
+                  <h5 class="font-semibold text-gray-700">First time here?</h5>
+                  <h5 v-on:click="goToSignUp" class="font-semibold text-blue-500 cursor-pointer">Create an account</h5>
+                </div>
               </div>
             </div>
           </form>
@@ -79,6 +95,64 @@ export default defineComponent({
   setup: () => {
     const count = ref(0)
     return { count }
+  },
+   data() {
+    return {
+      email: '',
+      password: '',
+      errorMessages: [],
+    }
+  },
+  watch: {
+    email(value){
+      this.validateEmail();
+    },
+    password(value){
+      this.validatePassword();
+    },
+  },
+  methods: {
+    // CHECK IF EMAIL IS WELL FORMATTED
+    validateEmail() {
+      let regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (!regexEmail.test(this.email))
+        this.errorMessages['email'] = 'Invalid Email Address';
+      else
+        this.errorMessages['email'] = '';
+    },
+
+    // CHECK IF PASSWORD IS LONG ENOUGH
+    validatePassword() {
+      if (this.password.length < 6 )
+        this.errorMessages['password'] = 'Too short password';
+      else
+        this.errorMessages['password'] = '';
+    },
+
+    // CHECK IF INPUTS ARE FILLED
+    checkInputs() {
+      if (!this.email || !this.password)
+        return false
+      if (this.errorMessages['email'] || this.errorMessages['password'])
+        return false
+      return true
+    },
+
+    // CALL SERVER FOR SIGN-IN
+    signIn() {
+      console.log(this.email + ' wants to sign-in')
+      // TODO: call server for sign in and manage errors
+    },
+
+    // MANAGE SIGN-IN WITH OFFICE
+    officeSignIn() {
+      alert("TODO")
+    },
+
+    // NAVIGATE TO SIGN UP PAGE
+    goToSignUp() {
+      alert("GHASSANE ALED")
+    }
   }
 })
 </script>
--- front/src/components/SignUp.vue
+++ front/src/components/SignUp.vue
@@ -8,44 +8,52 @@
         class="mx-auto overflow-hidden bg-white rounded-md shadow-lg max-w-md"
       >
         <div class="p-5 md:flex-1">
+          <!-- CARD HEADER -->
           <h3 class="my-4 text-2xl font-semibold text-gray-700">Create account</h3>
           <form action="#" class="flex flex-col space-y-5">
+            <!-- USERNAME INPUT -->
             <div class="flex flex-col space-y-1">
               <div class="flex items-center justify-between">
-                <label for="password" class="text-sm font-semibold text-gray-500">Username</label>
+                <label for="password" class="text-md font-semibold text-gray-500">Username</label>
               </div>
               <input
-                type="email"
-                id="email"
-                autofocus
+                type="text"
+                v-model="username"
                 class="px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"
               />
+              <span v-if="errorMessages.username" class="text-sm font-semibold text-red-500">{{errorMessages.username}}</span>
             </div>
+            <!-- EMAIL INPUT -->
             <div class="flex flex-col space-y-1">
               <div class="flex items-center justify-between">
-                <label for="password" class="text-sm font-semibold text-gray-500">Email</label>
+                <label for="password" class="text-md font-semibold text-gray-500">Email</label>
               </div>
               <input
-                type="email"
-                id="email"
-                autofocus
+                type="text"
+                v-model="email"
                 class="px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"
               />
+              <span v-if="errorMessages.email" class="text-sm font-semibold text-red-500">{{errorMessages.email}}</span>
             </div>
+            <!-- PASSWORD INPUT -->
             <div class="flex flex-col space-y-1">
               <div class="flex items-center justify-between">
-                <label for="password" class="text-sm font-semibold text-gray-500">Password</label>
+                <label for="password" class="text-md font-semibold text-gray-500">Password</label>
               </div>
               <input
                 type="password"
-                id="password"
+                v-model="password"
                 class="px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"
               />
+              <span v-if="errorMessages.password" class="text-sm font-semibold text-red-500">{{errorMessages.password}}</span>
             </div>
+            <!-- SIGN-UP BUTTON -->
             <div>
               <button
-                type="submit"
-                class="w-full px-4 py-2 text-lg font-semibold text-white transition-colors duration-300 bg-blue-500 rounded-md shadow hover:bg-blue-600 focus:outline-none focus:ring-blue-200 focus:ring-4"
+                type="button"
+                v-on:click="signUp"
+                v-bind:disabled="checkInputs() == false"
+                class="disabled:opacity-40 w-full px-4 py-2 text-lg font-semibold text-white transition-colors duration-300 bg-blue-500 rounded-md shadow hover:bg-blue-600 focus:outline-none focus:ring-blue-200 focus:ring-4"
               >
                 Sign-Up
               </button>
@@ -65,7 +73,69 @@ export default defineComponent({
   setup: () => {
     const count = ref(0)
     return { count }
-  }
+  },
+ data() {
+    return {
+      username: '',
+      email: '',
+      password: '',
+      errorMessages: [],
+    }
+  },
+  watch: {
+    username(value){
+      this.validateUsername();
+    },
+    email(value){
+      this.validateEmail();
+    },
+    password(value){
+      this.validatePassword();
+    },
+  },
+  methods: {
+    // CHECK IF USERNAME IS ALPHANUMERICAL
+    validateUsername() {
+      let regexUsername = /^[a-zA-Z0-9]*$/;
+      if (!regexUsername.test(this.username))
+        this.errorMessages['username'] = 'Invalid Username';
+      else
+        this.errorMessages['username'] = '';
+    },
+
+    // CHECK IF EMAIL IS WELL FORMATTED
+    validateEmail() {
+      let regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if (!regexEmail.test(this.email))
+        this.errorMessages['email'] = 'Invalid Email Address';
+      else
+        this.errorMessages['email'] = '';
+    },
+
+    // CHECK IF PASSWORD IS LONG ENOUGH
+    validatePassword() {
+      if (this.password.length < 6 )
+        this.errorMessages['password'] = 'Too short password';
+      else
+        this.errorMessages['password'] = '';
+    },
+
+    // CHECK IF INPUTS ARE FILLED AND VALIDS
+    checkInputs() {
+      if (!this.username || !this.email || !this.password)
+        return false
+
+      if (this.errorMessages['username'] || this.errorMessages['email'] || this.errorMessages['password'])
+        return false
+      return true
+    },
+
+    // CALL SERVER FOR SIGN-UP
+    signUp() {
+      console.log(this.username + ' wants to create an account')
+      // TODO: call server for registration here
+    }
+  },
 })
 </script>
 
