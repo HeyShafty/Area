9f7bb7cf56d894e585fe5977830291a5d6a96e29
Kylian Balan <kylian.balan@epitech.eu>
2021-01-23 18:06:49 +0100

    [SERVER] fix(azure app id).

--- server/src/config/azureConfig.js
+++ server/src/config/azureConfig.js
@@ -1,4 +1,4 @@
-const AZURE_CLIENT_ID = '5505e238-16d0-47b6-a859-995a9be67e36';
+const AZURE_CLIENT_ID = 'db074310-a1fb-45a3-8dd9-8462d3f688f8';
 const AZURE_ISSUER = 'https://login.microsoftonline.com/901cb4ca-b862-4029-9306-e5cd0f6d9f86/v2.0';
 const AZURE_DISCOVERY_URL = 'https://login.microsoftonline.com/901cb4ca-b862-4029-9306-e5cd0f6d9f86/discovery/v2.0/keys';
 
