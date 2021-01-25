3189128336cb16ceac733333722dee2e62c5c5b5
Rodolphe Dupuis <rodolphe.dupuis@epitech.eu>
2021-01-25 11:41:39 +0100

    Swagger updated for auth routes

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -9,10 +9,62 @@ const { STRATEGY_LOCAL_SIGN_IN, STRATEGY_LOCAL_SIGN_UP } = require('../passport/
 
 const router = express.Router();
 
+/**
+ * @swagger
+ *
+ * /auth/ping:
+ *   get:
+ *     summary: Checks if current session is valid.
+ *     responses:
+ *       200:
+ *         description: Authenticated.
+ *         content:
+ *           text/plain:
+ *             schema:
+ *               type: boolean
+ *       401:
+ *         description: Invalid session.
+ */
 router.get('/ping', isLoggedIn, (req, res) => {
     res.send(true);
 });
 
+/**
+ * @swagger
+ *
+ * /auth/sign-in:
+ *   post:
+ *      summary: Authenticate to the application.
+ *      produces:
+ *        - application/json
+ *      parameters:
+ *        - name: email
+ *          description: User email.
+ *          in: formData
+ *          required: true
+ *          type: string
+ *        - name: password
+ *          description: User password.
+ *          in: formData
+ *          required: true
+ *          type: string
+ *      responses:
+ *        200:
+ *          description: Authenticated.
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                properties:
+ *                  token:
+ *                    type: string
+ *        401:
+ *          description: Cannot find email or password.
+ *        409:
+ *          description: Wrong credentials.
+ *        500:
+ *          description: Error.
+ */
 router.post('/sign-in', (req, res, next) => {
     passport.authenticate(STRATEGY_LOCAL_SIGN_IN, (err, user, info) => {
         if (err) {
@@ -38,6 +90,47 @@ router.post('/sign-in', (req, res, next) => {
     })(req, res, next);
 });
 
+/**
+ * @swagger
+ *
+ * /auth/sign-up:
+ *   post:
+ *     summary: Register to the application.
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: email
+ *         description: User email.
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: password
+ *         description: User password.
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: fullName
+ *         in: formData
+ *         required: true
+ *         type: string
+ *         description: Username.
+ *     responses:
+ *       200:
+ *         description: Registered and authenticated.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *       400:
+ *         description: Cannot find email or password or fullName.
+ *       409:
+ *         description: A user with the given email already exists.
+ *       500:
+ *         description: Error.
+ */
 router.post('/sign-up', (req, res, next) => {
     passport.authenticate(STRATEGY_LOCAL_SIGN_UP, (err, user, info) => {
         if (err) {
@@ -63,6 +156,31 @@ router.post('/sign-up', (req, res, next) => {
     })(req, res, next);
 });
 
+/**
+ * @swagger
+ *
+ * /auth/office-jwt:
+ *   get:
+ *     summary: Sign-in or sign-up to the app using a Microsoft Azure OAuth 2.0 token.
+ *     parameters:
+ *       - in: header
+ *         name: Authorization
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: Authenticated.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *       400:
+ *         description: Missing token or bad token.
+ *       500:
+ *         description: Error.
+ */
 router.get('/office-jwt', (req, res, next) => {
     passport.authenticate(STRATEGY_OFFICE_JWT, (err, user) => {
         if (err) {
@@ -83,6 +201,16 @@ router.get('/office-jwt', (req, res, next) => {
     })(req, res, next);
 });
 
+/**
+ * @swagger
+ *
+ * /auth/logout:
+ *   get:
+ *     summary: Ends session.
+ *     responses:
+ *       302:
+ *         description: Session ended, redirecting to login page.
+ */
 router.get('/logout', (req, res) => {
     req.logout();
     // res.redirect(CLIENT_WEB_URI + '/auth/login'); // TODO: Watch out for the path
