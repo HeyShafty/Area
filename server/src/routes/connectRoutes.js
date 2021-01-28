5eab38403e4aa5a6a5c3f4811e37a8c7fca6a3ab
Rodolphe Dupuis <rodolphe.dupuis@epitech.eu>
2021-01-28 14:24:41 +0100

    passport google auth for kilio

--- server/src/routes/connectRoutes.js
+++ server/src/routes/connectRoutes.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const isLoggedIn = require("../passport/isLoggedIn");
+const passport = require('passport');
+const User = require('../models/User');
+const { CLIENT_WEB_URI } = require('../config/config');
+require('../passport/googleStrategy');
 
 const router = express.Router();
 
@@ -11,5 +15,48 @@ const router = express.Router();
 //         res.redirect('/'); // TODO: front home page.
 //     }
 // });
+
+/**
+ * @swagger
+ *
+ * /connect/google:
+ *   get:
+ *     summary: Login to Google service.
+ *     responses:
+ *       302:
+ *         description: Redirects to Google login page. On error, redirects to app home page.
+ *       401:
+ *         description: Not authenticated to the app.
+ *       500:
+ *         description: Error.
+ */
+router.get('/google', isLoggedIn, passport.authenticate('google', {
+    scope: [ 'email', 'profile', 'openid', 'https://www.googleapis.com/auth/youtube.readonly' ],
+    accessType: 'offline',
+    prompt: 'consent',
+    failureRedirect: CLIENT_WEB_URI + '/home'
+}));
+
+/**
+ * @swagger
+ *
+ * /connect/google/callback:
+ *   get:
+ *     summary: Callback used to catch Google authentication response.
+ *     responses:
+ *       302:
+ *         description: Redirects to app home page.
+ *       401:
+ *         description: Not authenticated to the app.
+ *       500:
+ *         description: Error.
+ */
+router.get('/google/callback', isLoggedIn, passport.authenticate('google', {
+    scope:
+        [ 'email', 'profile', 'openid', 'https://www.googleapis.com/auth/youtube.readonly' ],
+    accessType: 'offline',
+    successRedirect: CLIENT_WEB_URI + '/home',
+    failureRedirect: CLIENT_WEB_URI + '/home'
+}));
 
 module.exports = router;
