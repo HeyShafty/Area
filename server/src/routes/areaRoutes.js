f0c0f82bd8ce6066614e73d2e357cc7065da1b3d
Rodolphe Dupuis <rodolphe.dupuis@epitech.eu>
2021-02-16 18:25:03 +0100

    Adding post /area endpoint to create new Area

--- server/src/routes/areaRoutes.js
+++ server/src/routes/areaRoutes.js
@@ -3,33 +3,40 @@ const express = require('express');
 const protectedRequest = require('../passport/protectedRequest');
 const User = require('../models/User');
 const Area = require('../models/Area');
+const AREA_SERVICES = require('../services');
 
 const router = express.Router();
 
-router.post('/new', protectedRequest, async (req, res) => {
+router.post('/', protectedRequest, async(req, res) => {
     const { user } = req;
+    const actionService = AREA_SERVICES.find(service => service.name === req.body.action.service);
+    const reactionService = AREA_SERVICES.find(service => service.name === req.body.reaction.service);
+
+    if (!actionService) {
+        return res.status(400).send('Action service does not exist');
+    }
+    if (!reactionService) {
+        return res.status(400).send('Reaction service does not exist');
+    }
+
+    const actionName = actionService.actions.find(action => action.name === req.body.action.name);
+    const reactionName = reactionService.reactions.find(reaction => reaction.name === req.body.reaction.name);
+
+    if (!actionName) {
+        return res.status(400).send('Action does not exist');
+    }
+    if (!reactionName) {
+        return res.status(400).send('Reaction does not exist');
+    }
 
     const newArea = await Area.create({
         userId: user._id,
-        action: {
-            service: 'timer',
-            name: 'every_hour',
-            data: {
-                minute: parseInt(req.query.minute, 10)
-            }
-        },
-        reaction: {
-            service: 'microsoft',
-            name: 'send_mail',
-            data: {
-                to: 'nathan.lecorchet@epitech.eu',
-                body: 'Le pas de calais'
-            }
-        }
+        action: req.body.action,
+        reaction: req.body.reaction
     });
     console.log(newArea);
     return res.sendStatus(200);
-});
+})
 
 router.get('/', protectedRequest, async (req, res) => {
     const { user } = req;
