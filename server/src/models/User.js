d18ae823542710cd3912f67e3312b9b2a27efcc2
HeyShafty <nathan.lecorchet@epitech.eu>
2021-02-17 16:40:51 +0100

    User email can not be unique anymore.

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -21,8 +21,7 @@ const ServiceSchema = new Schema({
 const UserSchema = new Schema({
     email: {
         type: String,
-        required: true,
-        unique: true
+        required: true
     },
     password: {
         type: String,
