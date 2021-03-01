42ca18126d16cd274a9679f0a3824bbdecc80caf
SEBAÏ Ghassane <ghassane.sebai@epitech.eu>
2021-03-01 10:55:37 +0100

    [ADD] - sorry typo. Added </div> that ruined everything.

--- front/src/components/Home.vue
+++ front/src/components/Home.vue
@@ -3,86 +3,54 @@
     <!-- Hero Section -->
     <div class="relative overflow-hidden">
       <div class="max-w-7xl mx-auto">
-        <div
-          class="z-10 pb-8 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32"
-        >
+        <div class="z-10 pb-8 sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
           <div class="pt-6 px-4 sm:px-6 lg:px-8"></div>
-          <main
-            class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28"
-          >
+          <main class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
             <div class="sm:text-center lg:text-left">
-              <h1
-                class="text-4xl font-extrabold text-gray-900 sm:text-5xl md:text-6xl"
-              >
+              <h1 class="text-4xl font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
                 <span class="xl:inline">Connect your apps and</span>
-                <span class="text-indigo-600 xl:inline">
-                  automate workflows</span
-                >
+                <span class="text-indigo-600 xl:inline">automate workflows</span>
               </h1>
-              <p
-                class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
-              >
+              <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
                 Easy automation for busy people. <br />
                 From social media to emails, connect your favourite <br />
                 apps so you can save time to do the things you love.
               </p>
-              <div
-                class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start"
-              ></div>
+              <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start"></div>
             </div>
           </main>
         </div>
       </div>
       <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-        <img
-          class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-          src="../assets/background.jpg"
-          alt=""
-        />
+        <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="../assets/background.jpg" alt=""/>
       </div>
     </div>
-  </div>
 
   <!-- Get started section -->
   <div class="bg-gray-50">
-    <div
-      class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between"
-    >
-      <h2
-        class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl"
-      >
+    <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+      <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
         <span class="block">Ready to dive in?</span>
-        <span class="block text-indigo-600"
-          >Start using ChadArea for free today.</span
-        >
+        <span class="block text-indigo-600">Start using ChadArea for free today.</span>
       </h2>
       <div class="mt-8 flex lg:mt-0 lg:flex-shrink-0">
-        <router-link
-          to="/signup"
-          v-slot="{ href, route, navigate, isActive, isExactActive }"
-        >
+        <router-link to="/signup" v-slot="{ href, route, navigate, isActive, isExactActive }">
           <div class="inline-flex rounded-md shadow">
-            <p
-              class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
-            >
+            <p class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700">
               Sign Up
             </p>
           </div>
         </router-link>
-        <router-link
-          to="/signin"
-          v-slot="{ href, route, navigate, isActive, isExactActive }"
-        >
+        <router-link to="/signin" v-slot="{ href, route, navigate, isActive, isExactActive }">
           <div class="ml-3 inline-flex rounded-md shadow">
-            <p
-              class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50"
-            >
+            <p class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-indigo-50">
               Sign In
             </p>
           </div>
         </router-link>
       </div>
     </div>
+  </div>
 
   <!-- Features Section -->
   <div class="py-12 bg-white">
@@ -269,6 +237,7 @@
       <footer>
         <h6 class="text-sm font-medium -mb-8">© 2021 Chad Corporation</h6>
       </footer>
+  </div>
   </div>
 </template>
 
